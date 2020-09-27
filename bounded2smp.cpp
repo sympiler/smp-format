eb060b8af9f5e11af2b51fd6f2eea744ed4d488f
Shujian Qian <sewardqiansj@gmail.com>
2020-09-27 18:40:27 -0400

    Add error handling in ie2smp and bouded2smp

--- bounded2smp.cpp
+++ bounded2smp.cpp
@@ -4,6 +4,7 @@
 
 #include "utils.h"
 #include "mp_format_converter.h"
+#include "exceptions.h"
 
 
 using namespace format;
@@ -11,7 +12,16 @@ using namespace format;
 int main(int argc, const char *argv[]){
 
  std::map<std::string,std::string> qp_args;
- parse_args_bounded(argc, argv, qp_args);
+
+ try {
+  parse_args_bounded(argc, argv, qp_args);
+ } catch (const missing_arg_error& e) {
+  std::cerr << "Error: Missing argument: " << e.arg() << std::endl;
+  exit(1);
+ } catch (...) {
+  std::cerr << "An unknown error has occurred while parsing" << std::endl;
+  exit(1);
+ }
 
  std::string p_name, q_name, l_name, a_name, u_name;
  std::string output = "noname.yml";
@@ -29,13 +39,57 @@ int main(int argc, const char *argv[]){
  if(qp_args.find("output") != qp_args.end())
   output = qp_args["output"];
 
- auto *bf = load_bounded(p_name, q_name, l_name, a_name, u_name);
+ BoundedForm *bf = NULLPNTR;
+ try {
+  bf = load_bounded(p_name, q_name, l_name, a_name, u_name);
+ } catch (const read_file_error& e) {
+  std::cerr << "Error: Cannot read file: " << e.filename() << std::endl;
+  delete bf;
+  exit(1);
+ } catch (const mtx_format_error& e) {
+  std::cerr << "Error:\tMatrix format mismatch in " << e.filename() << std::endl
+            << "\tExpecting " << e.expected_format()
+            << " but got " << e.got_format()
+            << std::endl;
+  delete bf;
+  exit(1);
+ } catch (const mtx_arith_error& e) {
+  std::cerr << "Error:\tMatrix arithmetic mismatch in "
+            << e.filename() << std::endl
+            << "\tExpecting " << e.expected_arith()
+            << " but got " << e.got_arith()
+            << std::endl;
+  delete bf;
+  exit(1);
+ } catch (const mtx_header_error& e) {
+  std::cerr << "Error:\tInvalid header in " << e.filename() << std::endl
+            << "\t" << e.what() << std::endl;
+  delete bf;
+  exit(1);
+ } catch (...) {
+  std::cerr << "An unknown error has occurred while loading" << std::endl;
+  delete bf;
+  exit(1);
+ }
+
  Description test;
  std::string dtes=test.get_desc();
  auto *qfc = new QPFormatConverter(bf);
-//qfc->smp_->write("tmp2.yml");
+
  qfc->bounded_to_smp();
+ try {
  qfc->smp_->write(output);
+ } catch (const write_file_error& e) {
+  std::cerr << "Error: Unable to write to file " << e.filename();
+  delete bf;
+  delete qfc;
+  exit(1);
+ } catch (...) {
+  std::cerr << "An unknown error has occurred while writing" << std::endl;
+  delete bf;
+  delete qfc;
+  exit(1);
+ }
 
  delete bf;
  delete qfc;
